aedd56e60fbadb3101ee12179d81971fca06487e
eomain <eomain@protonmail.com>
2019-01-14 23:37:50 +0000

    builtin.h

--- ash/cd.c
+++ ash/cd.c
@@ -18,6 +18,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "builtin.h"
 #include "cd.h"
 #include "env.h"
 #include "io.h"
--- ash/sleep.c
+++ ash/sleep.c
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "builtin.h"
 #include "env.h"
 #include "io.h"
 #include "sleep.h"
