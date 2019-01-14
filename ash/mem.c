5fdabb014aaa86bda0d26b2892a9fc0017a8861e
eomain <eomain@protonmail.com>
2019-01-14 20:21:50 +0000

    add mem

---
+++ ash/mem.c
@@ -0,0 +1,55 @@
+/* Copyright 2018 eomain
+   this program is licensed under the 2-clause BSD license
+   see COPYING for the full license info
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ASH_MEM_ERR "fatal"
+
+#include "ash.h"
+#include "io.h"
+#include "mem.h"
+
+void *ash_alloc(size_t n)
+{
+    assert( n > 0 );
+
+    void *m = malloc(n);
+    if (!m){
+        ash_set_status(errno);
+        ash_abort(strerror(errno));
+    }
+    return m;
+}
+
+void *ash_realloc(void *m, size_t n)
+{
+    assert( m && n > 0 );
+    m = realloc(m, n);
+    if (!m){
+        ash_set_status(errno);
+        ash_abort(strerror(errno));
+    }
+    return m;
+}
+
+void ash_free(void *m)
+{
+    if (m)
+        free(m);
+}
