f78d165b48efa90b5c07e11fd571cc61d3fe9ff0
eomain <eomain@protonmail.com>
2019-01-15 01:10:56 +0000

    source command

---
+++ ash/source.c
@@ -0,0 +1,40 @@
+/* Copyright 2018 eomain
+   this program is licensed under the 2-clause BSD license
+   see COPYING for the full license info
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+   FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+   CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "builtin.h"
+#include "env.h"
+#include "script.h"
+#include "source.h"
+
+const char *ash_source_usage(void)
+{
+    return "read and execute commands from file";
+}
+
+int ash_source(int argc, const char * const *argv)
+{
+    int status = 0;
+
+    for (int i = 1; i < argc; ++i){
+        if (!ash_script_load(argv[i]))
+            status = -1;
+    }
+
+    return status;
+}
