32f05c9a39823bfcd4ad250ce5bc9cfdab5f876a
eomain <eomain@protonmail.com>
2019-01-15 04:40:33 +0000

    test 0 - hello world

---
+++ tests/0.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env ash
+
+# Copyright 2018 eomain
+# this program is licensed under the 2-clause BSD license
+# see COPYRIGHT for the full license info
+
+# ash (acorn shell) install script
+
+# print hello world
+echo "hello world!"
