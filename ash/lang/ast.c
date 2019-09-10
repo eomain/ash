251d19ca9654cbd572b5ebe333e352f6b823daec
eomain <eomain@protonmail.com>
2019-09-10 16:12:43 +0100

    ast destroyers

--- ash/lang/ast.c
+++ ash/lang/ast.c
@@ -22,6 +22,9 @@
 #include "ash/var.h"
 #include "ash/lang/ast.h"
 
+void ast_expr_destroy(struct ast_expr *);
+void ast_else_destroy(struct ast_else *);
+
 struct ast_scope *ast_scope_new(const char *id)
 {
     struct ast_scope *scope;
@@ -151,6 +154,20 @@ struct ast_literal *ast_literal_closure(struct ast_function *value)
     return literal;
 }
 
+void ast_literal_destroy(struct ast_literal *literal)
+{
+    switch (literal->type) {
+        case AST_LITERAL_STR:
+            ash_free((char *)literal->value.string);
+            break;
+        case AST_LITERAL_RANGE:
+            ast_range_destroy(literal->value.range);
+            break;
+    }
+
+    ash_free(literal);
+}
+
 static inline struct ast_value *ast_value_new(void)
 {
     return ash_zalloc(sizeof (struct ast_value));
@@ -174,6 +191,16 @@ struct ast_value *ast_value_literal(struct ast_literal *literal)
     return value;
 }
 
+void ast_value_destroy(struct ast_value *value)
+{
+    if (value->type == AST_VALUE_VAR)
+        ast_var_destroy(value->value.var);
+    else if (value->type == AST_VALUE_LITERAL)
+        ast_literal_destroy(value->value.literal);
+
+    ash_free(value);
+}
+
 struct ast_unary *ast_unary_new(enum ast_unary_op op, struct ast_expr *expr)
 {
     struct ast_unary *unary;
@@ -181,6 +208,13 @@ struct ast_unary *ast_unary_new(enum ast_unary_op op, struct ast_expr *expr)
     unary->op = op;
     unary->expr = expr;
     return unary;
+}
+
+void ast_unary_destroy(struct ast_unary *unary)
+{
+    if (unary->expr)
+        ast_expr_destroy(unary->expr);
+    ash_free(unary);
 }
 
 struct ast_binary *ast_binary_new(enum ast_binary_op op, struct ast_expr *e1,
@@ -194,6 +228,15 @@ struct ast_binary *ast_binary_new(enum ast_binary_op op, struct ast_expr *e1,
     return binary;
 }
 
+void ast_binary_destroy(struct ast_binary *binary)
+{
+    if (binary->e1)
+        ast_expr_destroy(binary->e1);
+    if (binary->e2)
+        ast_expr_destroy(binary->e2);
+    ash_free(binary);
+}
+
 struct ast_cmp *ast_cmp_new(enum ast_cmp_op op, struct ast_expr *e1,
                             struct ast_expr *e2)
 {
@@ -203,6 +246,15 @@ struct ast_cmp *ast_cmp_new(enum ast_cmp_op op, struct ast_expr *e1,
     cmp->e1 = e1;
     cmp->e2 = e2;
     return cmp;
+}
+
+void ast_cmp_destroy(struct ast_cmp *cmp)
+{
+    if (cmp->e1)
+        ast_expr_destroy(cmp->e1);
+    if (cmp->e2)
+        ast_expr_destroy(cmp->e2);
+    ash_free(cmp);
 }
 
 struct ast_logical *
@@ -216,6 +268,15 @@ ast_logical_new(enum ast_logical_op op, struct ast_expr *e1, struct ast_expr *e2
     return logical;
 }
 
+void ast_logical_destroy(struct ast_logical *logical)
+{
+    if (logical->e1)
+        ast_expr_destroy(logical->e1);
+    if (logical->e2)
+        ast_expr_destroy(logical->e2);
+    ash_free(logical);
+}
+
 struct ast_composite *ast_composite_new(struct ast_expr *expr, size_t length)
 {
     struct ast_composite *comp;
@@ -223,6 +284,13 @@ struct ast_composite *ast_composite_new(struct ast_expr *expr, size_t length)
     comp->expr = expr;
     comp->length = length;
     return comp;
+}
+
+void ast_composite_destroy(struct ast_composite *comp)
+{
+    if (comp->expr)
+        ast_expr_destroy(comp->expr);
+    ash_free(comp);
 }
 
 struct ast_expr *ast_expr_new(enum ast_expr_type type, void *value)
@@ -235,7 +303,19 @@ struct ast_expr *ast_expr_new(enum ast_expr_type type, void *value)
     return expr;
 }
 
-struct ast_assign *ast_assign_new(bool local, struct ast_var *var, struct ast_expr *expr)
+void ast_expr_destroy(struct ast_expr *expr)
+{
+    switch (expr->type) {
+
+    }
+
+    if (expr->next)
+        ast_expr_destroy(expr->next);
+    ash_free(expr);
+}
+
+struct ast_assign *ast_assign_new(bool local, struct ast_var *var,
+                                  struct ast_expr *expr)
 {
     struct ast_assign *assign;
     assign = ash_alloc(sizeof *assign);
@@ -245,12 +325,28 @@ struct ast_assign *ast_assign_new(bool local, struct ast_var *var, struct ast_ex
     return assign;
 }
 
+void ast_assign_destroy(struct ast_assign *assign)
+{
+    if (assign->var)
+        ast_var_destroy(assign->var);
+    if (assign->expr)
+        ast_expr_destroy(assign->expr);
+    ash_free(assign);
+}
+
 struct ast_bool_expr *ast_bool_expr_new(struct ast_expr *expr)
 {
     struct ast_bool_expr *bexpr;
     bexpr = ash_alloc(sizeof *bexpr);
     bexpr->expr = expr;
     return bexpr;
+}
+
+void ast_bool_expr_destroy(struct ast_bool_expr *bexpr)
+{
+    if (bexpr->expr)
+        ast_expr_destroy(bexpr->expr);
+    ash_free(bexpr);
 }
 
 struct ast_ternary *ast_ternary_new(struct ast_bool_expr *cond,
@@ -264,8 +360,20 @@ struct ast_ternary *ast_ternary_new(struct ast_bool_expr *cond,
     return ternary;
 }
 
+void ast_ternary_destroy(struct ast_ternary *ternary)
+{
+    if (ternary->cond)
+        ast_bool_expr_destroy(ternary->cond);
+    if (ternary->e1)
+        ast_expr_destroy(ternary->e1);
+    if (ternary->e2)
+        ast_expr_destroy(ternary->e2);
+    ash_free(ternary);
+}
+
 struct ast_case *
-ast_case_new(struct ast_expr *expr, struct ast_expr *eval, struct ast_case *next)
+ast_case_new(struct ast_expr *expr, struct ast_expr *eval,
+             struct ast_case *next)
 {
     struct ast_case *ecase;
     ecase = ash_alloc(sizeof *ecase);
@@ -273,6 +381,17 @@ ast_case_new(struct ast_expr *expr, struct ast_expr *eval, struct ast_case *next
     ecase->eval = eval;
     ecase->next = next;
     return ecase;
+}
+
+void ast_case_destroy(struct ast_case *ecase)
+{
+    if (ecase->expr)
+        ast_expr_destroy(ecase->expr);
+    if (ecase->eval)
+        ast_expr_destroy(ecase->eval);
+    if (ecase->next)
+        ast_case_destroy(ecase->next);
+    ash_free(ecase);
 }
 
 struct ast_match *
@@ -287,6 +406,17 @@ ast_match_new(struct ast_expr *expr, struct ast_case *ecase,
     return match;
 }
 
+void ast_match_destroy(struct ast_match *match)
+{
+    if (match->expr)
+        ast_expr_destroy(match->expr);
+    if (match->ecase)
+        ast_case_destroy(match->ecase);
+    if (match->otherwise)
+        ast_expr_destroy(match->otherwise);
+    ash_free(match);
+}
+
 struct ast_stm *
 ast_stm_new(enum ast_node_type type, void *node)
 {
@@ -296,6 +426,17 @@ ast_stm_new(enum ast_node_type type, void *node)
     stm->node = node;
     stm->next = NULL;
     return stm;
+}
+
+void ast_stm_destroy(struct ast_stm *stm)
+{
+    switch (stm->type) {
+
+    }
+
+    if (stm->next)
+        ast_stm_destroy(stm->next);
+    ash_free(stm);
 }
 
 struct ast_command *ast_command_new(struct ast_expr *expr, size_t length)
@@ -308,6 +449,13 @@ struct ast_command *ast_command_new(struct ast_expr *expr, size_t length)
     return command;
 }
 
+void ast_command_destroy(struct ast_command *command)
+{
+    if (command->expr)
+        ast_expr_destroy(command->expr);
+    ash_free(command);
+}
+
 struct ast_call *ast_call_new(struct ast_var *var, struct ast_composite *args)
 {
     struct ast_call *call;
@@ -315,6 +463,15 @@ struct ast_call *ast_call_new(struct ast_var *var, struct ast_composite *args)
     call->var = var;
     call->args = args;
     return call;
+}
+
+void ast_call_destroy(struct ast_call *call)
+{
+    if (call->var)
+        ast_var_destroy(call->var);
+    if (call->args)
+        ast_composite_destroy(call->args);
+    ash_free(call);
 }
 
 struct ast_if *ast_if_new(struct ast_bool_expr *expr, struct ast_stm *stm,
@@ -328,10 +485,30 @@ struct ast_if *ast_if_new(struct ast_bool_expr *expr, struct ast_stm *stm,
     return ast_if;
 }
 
+void ast_if_destroy(struct ast_if *ast_if)
+{
+    if (ast_if->cond)
+        ast_bool_expr_destroy(ast_if->cond);
+    if (ast_if->stm)
+        ast_stm_destroy(ast_if->stm);
+    if (ast_if->else_t)
+        ast_else_destroy(ast_if->else_t);
+    ash_free(ast_if);
+}
+
 static struct ast_else *
 ast_else(void)
 {
     return ash_zalloc(sizeof (struct ast_else));
+}
+
+void ast_else_destroy(struct ast_else *ae)
+{
+    if (ae->type == AST_ELSE)
+        ast_stm_destroy(ae->stm.stm);
+    else if (ae->type == AST_ELSE_IF)
+        ast_if_destroy(ae->stm.if_t);
+    ash_free(ae);
 }
 
 struct ast_else *
@@ -364,6 +541,15 @@ ast_while_new(struct ast_bool_expr *cond, struct ast_stm *stm)
     return ast_while;
 }
 
+void ast_while_destroy(struct ast_while *ast_while)
+{
+    if (ast_while->cond)
+        ast_bool_expr_destroy(ast_while->cond);
+    if (ast_while->stm)
+        ast_stm_destroy(ast_while->stm);
+    ash_free(ast_while);
+}
+
 struct ast_for *
 ast_for_new(struct ast_var *var, struct ast_expr *expr, struct ast_stm *stm)
 {
@@ -375,6 +561,17 @@ ast_for_new(struct ast_var *var, struct ast_expr *expr, struct ast_stm *stm)
     return ast_for;
 }
 
+void ast_for_destroy(struct ast_for *ast_for)
+{
+    if (ast_for->var)
+        ast_var_destroy(ast_for->var);
+    if (ast_for->expr)
+        ast_expr_destroy(ast_for->expr);
+    if (ast_for->stm)
+        ast_stm_destroy(ast_for->stm);
+    ash_free(ast_for);
+}
+
 struct ast_param *ast_param_new(const char *id)
 {
     struct ast_param *param;
@@ -382,6 +579,15 @@ struct ast_param *ast_param_new(const char *id)
     param->id = id;
     param->next = NULL;
     return param;
+}
+
+void ast_param_destroy(struct ast_param *param)
+{
+    if (param->id)
+        ash_free(param);
+    if (param->next)
+        ast_param_destroy(param->next);
+    ash_free(param);
 }
 
 struct ast_function *
@@ -395,6 +601,17 @@ ast_function_new(const char *id, struct ast_param *param, struct ast_stm *stm)
     return function;
 }
 
+void ast_function_destroy(struct ast_function *function)
+{
+    if (function->id)
+        ash_free((char *)function->id);
+    if (function->param)
+        ast_param_destroy(function->param);
+    if (function->stm)
+        ast_stm_destroy(function->stm);
+    ash_free(function);
+}
+
 struct ast_return *
 ast_return_new(struct ast_expr *expr)
 {
@@ -402,6 +619,13 @@ ast_return_new(struct ast_expr *expr)
     ret = ash_alloc(sizeof *ret);
     ret->expr = expr;
     return ret;
+}
+
+void ast_return_destroy(struct ast_return *ret)
+{
+    if (ret->expr)
+        ast_expr_destroy(ret->expr);
+    ash_free(ret);
 }
 
 struct ast_use *
@@ -413,6 +637,13 @@ ast_use_new(struct ast_path *path)
     return use;
 }
 
+void ast_use_destroy(struct ast_use *use)
+{
+    if (use->path)
+        ast_path_destroy(use->path);
+    ash_free(use);
+}
+
 struct ast_module *
 ast_module_new(const char *name, struct ast_stm *stm)
 {
@@ -421,4 +652,13 @@ ast_module_new(const char *name, struct ast_stm *stm)
     module->name = name;
     module->stm = stm;
     return module;
+}
+
+void ast_module_destroy(struct ast_module *module)
+{
+    if (module->name)
+        ash_free((char *)module->name);
+    if (module->stm)
+        ast_stm_destroy(module->stm);
+    ash_free(module);
 }
